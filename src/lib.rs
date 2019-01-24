b9fe70e62ae010125da4d7999527a5ad85d94466
Alexander Thaller <alexander.thaller@trivago.com>
2019-01-24 22:19:58 +0100

    Added method that communicates when a new reuquest was received. This is mostly usefull to update metrics before sending them out to a requester.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,18 @@
 #![deny(missing_docs)]
 
 use std::net::SocketAddr;
+use std::thread;
 
-use log::info;
+use log::{
+    error,
+    info,
+};
 
+use crossbeam_channel::{
+    bounded,
+    Receiver,
+    Sender,
+};
 use hyper::{
     rt::{
         self,
@@ -13,6 +22,7 @@ use hyper::{
     },
     service::service_fn_ok,
     Body,
+    Error as HyperError,
     Method,
     Response,
     Server,
@@ -22,47 +32,103 @@ use prometheus::{
     TextEncoder,
 };
 
+/// Errors that can happen when the prometheus exporter gets started.
+pub enum StartError {
+    /// Hyper related errors.
+    HyperError(HyperError),
+}
+
+impl From<HyperError> for StartError {
+    fn from(err: HyperError) -> Self {
+        StartError::HyperError(err)
+    }
+}
+
 /// Struct that holds everything together.
-pub struct PrometheusExporter {}
+pub struct PrometheusExporter;
+
+/// Struct that will be sent when there is a new request in the run_and_nofity
+/// function.
+pub struct NewRequest;
+
+/// Struct that has to be sent when the update was finished and the request
+/// should send the metrics to the requester.
+pub struct FinishedUpdate;
 
 impl PrometheusExporter {
     /// Start the prometheus exporter and bind the hyper http server to the
     /// given socket.
-    pub fn run(addr: &SocketAddr) {
+    pub fn run(addr: &SocketAddr) -> Result<(), StartError> {
         let service = move || {
             let encoder = TextEncoder::new();
 
             service_fn_ok(move |req| match (req.method(), req.uri().path()) {
-                (&Method::GET, "/metrics") => {
-                    let metric_families = prometheus::gather();
-                    let mut buffer = vec![];
-                    encoder.encode(&metric_families, &mut buffer).unwrap();
-
-                    Response::new(Body::from(buffer))
-                }
-                _ => {
-                    let message = "try /metrics for metrics\n";
-                    Response::builder()
-                        .status(301)
-                        .body(Body::from(message))
-                        .unwrap()
-                }
+                (&Method::GET, "/metrics") => PrometheusExporter::send_metrics(&encoder),
+                _ => PrometheusExporter::send_redirect(),
             })
         };
 
-        let server = Server::bind(&addr)
+        let server = Server::try_bind(&addr)?
             .serve(service)
-            .map_err(|e| eprintln!("server error: {}", e));
+            .map_err(|e| error!("problem while serving metrics: {}", e));
 
         info!("Listening on http://{}", addr);
 
         rt::run(server);
-    }
-}
 
-/// Trait to pass an updater to PrometheusExporte that will be used to update
-/// the metrics for each requst.
-pub trait MetricUpdater {
-    /// Runs the update on the metrics.
-    fn update(&self);
+        Ok(())
+    }
+
+    /// Start the prometheus exporter, bind the hyper http server to the given
+    /// socket and send messages when there are new requests for metrics.
+    /// This is usefull if metrics should be updated everytime there is a
+    /// requests.
+    pub fn run_and_notify(addr: SocketAddr) -> (Receiver<NewRequest>, Sender<FinishedUpdate>) {
+        let (request_sender, request_receiver) = bounded(0);
+        let (finished_sender, finished_receiver) = bounded(0);
+
+        thread::spawn(move || {
+            let service = move || {
+                let encoder = TextEncoder::new();
+                let request_sender = request_sender.clone();
+                let finished_receiver = finished_receiver.clone();
+
+                service_fn_ok(move |req| match (req.method(), req.uri().path()) {
+                    (&Method::GET, "/metrics") => {
+                        request_sender.send(NewRequest {}).unwrap();
+                        finished_receiver.recv().unwrap();
+
+                        PrometheusExporter::send_metrics(&encoder)
+                    }
+                    _ => PrometheusExporter::send_redirect(),
+                })
+            };
+
+            let server = Server::bind(&addr)
+                .serve(service)
+                .map_err(|e| error!("problem while serving metrics: {}", e));
+
+            info!("Listening on http://{}", addr);
+
+            rt::run(server);
+        });
+
+        (request_receiver, finished_sender)
+    }
+
+    fn send_metrics(encoder: &TextEncoder) -> Response<Body> {
+        let metric_families = prometheus::gather();
+        let mut buffer = vec![];
+        encoder.encode(&metric_families, &mut buffer).unwrap();
+
+        Response::new(Body::from(buffer))
+    }
+
+    fn send_redirect() -> Response<Body> {
+        let message = "try /metrics for metrics\n";
+        Response::builder()
+            .status(301)
+            .body(Body::from(message))
+            .unwrap()
+    }
 }
