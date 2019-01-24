7a922bcf380ca0e95ee513e655e9466fa73ae3ac
Alexander Thaller <alexander.thaller@trivago.com>
2019-01-24 14:43:56 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::net::SocketAddr;
+
+use log::info;
+
+use hyper::{
+    rt::{
+        self,
+        Future,
+    },
+    service::service_fn_ok,
+    Body,
+    Method,
+    Response,
+    Server,
+};
+use prometheus::{
+    Encoder,
+    TextEncoder,
+};
+
+pub struct PrometheusExporter {}
+
+impl PrometheusExporter {
+    pub fn run(self, addr: &SocketAddr) {
+        let service = move || {
+            let encoder = TextEncoder::new();
+
+            service_fn_ok(move |req| match (req.method(), req.uri().path()) {
+                (&Method::GET, "/metrics") => {
+                    let metric_families = prometheus::gather();
+                    let mut buffer = vec![];
+                    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+                    Response::new(Body::from(buffer))
+                }
+                _ => {
+                    let message = "try /metrics for metrics\n";
+                    Response::builder()
+                        .status(301)
+                        .body(Body::from(message))
+                        .unwrap()
+                }
+            })
+        };
+
+        let server = Server::bind(&addr)
+            .serve(service)
+            .map_err(|e| eprintln!("server error: {}", e));
+
+        info!("Listening on http://{}", addr);
+
+        rt::run(server);
+    }
+}
