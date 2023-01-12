9d4120fcef803ec9ccb4dc5a347f4117e85dd8f0
Alexander Thaller <alexander@thaller.ws>
2023-01-12 09:46:55 +0100

    Fix clippy lints (#40)
    Co-authored-by: Alexander Thaller <alexander.thaller@sva.de>

--- examples/custom_registry.rs
+++ examples/custom_registry.rs
@@ -37,7 +37,7 @@ fn main() {
     builder.start().expect("can not start exporter");
 
     // Get metrics from exporter
-    let body = reqwest::blocking::get(&format!("http://{}/metrics", addr_raw))
+    let body = reqwest::blocking::get(format!("http://{addr_raw}/metrics"))
         .expect("can not get metrics from exporter")
         .text()
         .expect("can not body text from request");
